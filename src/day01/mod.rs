94924f7877a75c6ac999196e722e2613dea5d764
Etos2 <etos2etos2@gmail.com>
2022-12-01 22:16:48 +1100

    error handling + big enum

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,21 +1,34 @@
-pub fn challenge_a(input: &str) {
-    let max = input
+use anyhow::{Context, Result};
+
+pub fn challenge_a(input: &str) -> Result<()> {
+    let sums = input
         .split("\n\n")
-        .map(|g| g.lines().map(|l| l.parse::<i32>().unwrap()).sum::<i32>())
-        .max()
-        .unwrap();
+        .map(|g| {
+            g.lines()
+                .map(|l| l.parse::<i32>().context(format!("unable to parse \"{l}\"")))
+                .sum::<Result<i32>>()
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    let max = sums.iter().max().context("file is empty")?;
 
     println!("{}", max);
+    Ok(())
 }
 
-pub fn challenge_b(input: &str) {
+pub fn challenge_b(input: &str) -> Result<()> {
     let mut sums = input
         .split("\n\n")
-        .map(|g| g.lines().map(|l| l.parse::<i32>().unwrap()).sum::<i32>())
-        .collect::<Vec<_>>();
+        .map(|g| {
+            g.lines()
+                .map(|l| l.parse::<i32>().context(format!("unable to parse \"{l}\"")))
+                .sum::<Result<i32>>()
+        })
+        .collect::<Result<Vec<_>>>()?;
 
     sums.sort_by(|a, b| b.cmp(a));
     let max = sums.iter().take(3).sum::<i32>();
 
     println!("{}", max);
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,86 @@
 mod day01;
 
-use anyhow::Result;
+use anyhow::{bail, Result, ensure};
 use itertools::Itertools;
+use strum_macros::EnumString;
 
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, EnumString)]
+#[strum(ascii_case_insensitive)]
 enum Challenge {
     Day01a,
     Day01b,
-}
-
-impl FromStr for Challenge {
-    type Err = ();
-
-    fn from_str(input: &str) -> Result<Challenge, Self::Err> {
-        match input.to_lowercase().as_ref() {
-            "day01a" => Ok(Challenge::Day01a),
-            "day01b" => Ok(Challenge::Day01b),
-            _ => Err(()),
-        }
-    }
+    Day02a,
+    Day02b,
+    Day03a,
+    Day03b,
+    Day04a,
+    Day04b,
+    Day05a,
+    Day05b,
+    Day06a,
+    Day06b,
+    Day07a,
+    Day07b,
+    Day08a,
+    Day08b,
+    Day09a,
+    Day09b,
+    Day10a,
+    Day10b,
+    Day11a,
+    Day11b,
+    Day12a,
+    Day12b,
+    Day13a,
+    Day13b,
+    Day14a,
+    Day14b,
+    Day15a,
+    Day15b,
+    Day16a,
+    Day16b,
+    Day17a,
+    Day17b,
+    Day18a,
+    Day18b,
+    Day19a,
+    Day19b,
+    Day20a,
+    Day20b,
+    Day21a,
+    Day21b,
+    Day22a,
+    Day22b,
+    Day23a,
+    Day23b,
+    Day24a,
+    Day24b,
+    Day25a,
+    Day25b,
 }
 
 fn main() -> Result<()> {
     let args = std::env::args().skip(1);
+
+    ensure!(
+        args.len() % 2 == 0,
+        "incorrect amount of arguments (expected {} found {})",
+        args.len() + 1,
+        args.len()
+    );
+
     for (day, path) in args.tuples() {
         if let Ok(c) = Challenge::from_str(&day) {
             let data = std::fs::read_to_string(&path)?;
             match c {
-                Challenge::Day01a => day01::challenge_a(&data),
-                Challenge::Day01b => day01::challenge_b(&data),
+                Challenge::Day01a => day01::challenge_a(&data)?,
+                Challenge::Day01b => day01::challenge_b(&data)?,
+                _ => bail!("challenge not complete yet!"),
             }
         }
     }
+
     Ok(())
 }
